b3cfffaab8f004a2a2bc68f12de97ce5905abe0e
Lucas <lucasblanco31@gmail.com>
2020-06-14 20:45:38 -0300

    Se corrigio punto1 consulta

--- Nuevo/consultas/punto1.sql
+++ Nuevo/consultas/punto1.sql
@@ -1,10 +1,10 @@
-select nombre, apellido, titulo
+(select nombre, apellido, titulo, count(*) as 'Leidas', sum(if(megusta=1,1,0))as 'Me gusta' from reg_navegacion join
+(select nombre, apellido, titulo, noticia_mod.fecha, noticia_mod.idnoticia
 from usuario_actual, usuario_mod, noticia_actual, noticia_mod
 where usuario_actual.id_usuario = usuario_mod.id
 and noticia_actual.id_noticia = noticia_mod.id
 and noticia_mod.autor = usuario_actual.idusuario_actual
+group by usuario_actual.id_usuario) autnot 
+on reg_navegacion.id_noticia = autnot.idnoticia
+group by reg_navegacion.id_noticia)
 
-##### PARA SABER CUANTAS LEIDAS TIENE CADA UNA
-#select id_noticia, count(*) as 'Leidas' from reg_navegacion
-#group by id_noticia
-#####
