61de21e8aebcca11323a1df61f19570a108caaed
Lucas <lucasblanco31@gmail.com>
2020-06-14 21:18:15 -0300

    Falta mostrar la noticia con mas leidas por autor en consulta 1

--- Nuevo/consultas/punto1.sql
+++ Nuevo/consultas/punto1.sql
@@ -1,10 +1,14 @@
-(select nombre, apellido, titulo, count(*) as 'Leidas', sum(if(megusta=1,1,0))as 'Me gusta' from reg_navegacion join
-(select nombre, apellido, titulo, noticia_mod.fecha, noticia_mod.idnoticia
+select autnot.idusuario, nombre, apellido, titulo, autnot.Fecha, count(*) as 'Leidas', sum(if(megusta=1,1,0))as 'Me gusta' from reg_navegacion join
+(select nombre, apellido, titulo, usuario_mod.idusuario as idusuario, noticia_mod.fecha as Fecha, noticia_mod.idnoticia
 from usuario_actual, usuario_mod, noticia_actual, noticia_mod
 where usuario_actual.id_usuario = usuario_mod.id
 and noticia_actual.id_noticia = noticia_mod.id
 and noticia_mod.autor = usuario_actual.idusuario_actual
-group by usuario_actual.id_usuario) autnot 
+group by usuario_actual.id_usuario
+) autnot 
 on reg_navegacion.id_noticia = autnot.idnoticia
-group by reg_navegacion.id_noticia)
+group by reg_navegacion.id_noticia
+
+
+
 
