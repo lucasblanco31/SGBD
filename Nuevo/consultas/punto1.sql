3ceb2d73a7ff786e0af7ac172ba93bc14e45cfdf
Lucas Blanco <lucasblanco31@gmail.com>
2020-06-19 20:45:54 -0300

    Se arreglo punto1 y punto3

--- Nuevo/consultas/punto1.sql
+++ Nuevo/consultas/punto1.sql
@@ -1,14 +1,28 @@
-select autnot.idusuario, nombre, apellido, titulo, autnot.Fecha, count(*) as 'Leidas', sum(if(megusta=1,1,0))as 'Me gusta' from reg_navegacion join
-(select nombre, apellido, titulo, usuario_mod.idusuario as idusuario, noticia_mod.fecha as Fecha, noticia_mod.idnoticia
+/*
+b1) Elaborar una consulta SQL que genere un listado que contenga todos los autores de noticias 
+con su nombre y apellido y que además muestre cual es la noticia propia (titulo) mas leida, 
+de que fecha y cuantos "me gusta" tiene esa noticia. Si varias noticias tienen la misma 
+cantidad maxima de lecturas poner una cualquiera de ellas
+
+NOMBREAUTOR APELLIDOAUTOR TITULO FECHA MEGUSTA
+*/
+select prueba.nom as 'NOMBRE', prueba.ape as 'APELLIDO', prueba.ti as 'TITULO', prueba.fe as 'FECHA', max(prueba.le) as 'LEIDAS', prueba.mg as 'MEGUSTA'
+from reg_navegacion
+join
+(select autnot.idusuario as ID, nombre as nom, apellido as ape, titulo as ti, autnot.Fecha as fe, count(*) as le, sum(if(megusta=1,1,0))as mg
+from reg_navegacion 
+join 
+(
+select nombre, apellido, titulo, usuario_mod.idusuario as idusuario, noticia_mod.fecha as Fecha, 
+noticia_mod.idnoticia
 from usuario_actual, usuario_mod, noticia_actual, noticia_mod
 where usuario_actual.id_usuario = usuario_mod.id
 and noticia_actual.id_noticia = noticia_mod.id
 and noticia_mod.autor = usuario_actual.idusuario_actual
-group by usuario_actual.id_usuario
 ) autnot 
 on reg_navegacion.id_noticia = autnot.idnoticia
-group by reg_navegacion.id_noticia
-
+group by reg_navegacion.id_noticia) prueba
+group by prueba.ID
 
 
 
--- Nuevo/consultas/punto2.sql
+++ Nuevo/consultas/punto2.sql
@@ -1,3 +1,13 @@
-select distinct nombre, apellido, tema from noticia_mod, usuario_mod, usuario_actual
+/*
+b2) Elaborar una consulta SQL que genere un listado que contenga todas las combinaciones 
+autor-tema indicando cuantas publicaciones tiene el autor sobre ese tema, si no tiene ninguna 
+poner 0 (cero) ordenar por autor y cantidad de publicaciones.
+NOMBREAUTOR APELLIDOAUTOR TEMA CANTIDAD
+*/
+
+select nombre, apellido, tema, count(*) as 'CantTema', autor 
+from noticia_mod, usuario_mod, usuario_actual
 where autor = usuario_actual.idusuario_actual
 and usuario_actual.id_usuario = usuario_mod.id
+group by nombre, apellido, tema
+order by nombre, apellido, CantTema desc
