b49644d31a8dd04e9e49f628216774df44ca2624
Lucas Blanco <lucasblanco31@gmail.com>
2020-06-23 17:31:45 -0300

    fin punto 1

--- Nuevo/consultas/punto1.sql
+++ Nuevo/consultas/punto1.sql
@@ -6,21 +6,23 @@ cantidad maxima de lecturas poner una cualquiera de ellas
 
 NOMBREAUTOR APELLIDOAUTOR TITULO FECHA MEGUSTA
 */
-select prueba.nom as 'NOMBRE', prueba.ape as 'APELLIDO', prueba.ti as 'TITULO', prueba.fe as 'FECHA', max(prueba.le) as 'LEIDAS', prueba.mg as 'MEGUSTA'
+select prueba.nom as 'NOMBRE', prueba.ape as 'APELLIDO', prueba.ti as 'TITULO', prueba.fe as 'FECHA', prueba.mg as 'MEGUSTA'
 from 
 (select autnot.idusuario as ID, nombre as nom, apellido as ape, titulo as ti, autnot.Fecha as fe, count(*) as le, sum(if(megusta=1,1,0))as mg
 from reg_navegacion 
 join 
 (
 select nombre, apellido, titulo, usuario_mod.idusuario as idusuario, noticia_mod.fecha as Fecha, 
-noticia_mod.idnoticia
+noticia_mod.idnoticia as idnot
 from usuario_actual, usuario_mod, noticia_actual, noticia_mod
 where usuario_actual.id_usuario = usuario_mod.id
 and noticia_actual.id_noticia = noticia_mod.id
 and noticia_mod.autor = usuario_actual.idusuario_actual
 ) autnot 
-on reg_navegacion.id_noticia = autnot.idnoticia
-group by reg_navegacion.id_noticia) prueba
+on reg_navegacion.id_noticia = autnot.idnot
+group by reg_navegacion.id_noticia
+#Al ordernar por cantidad de leidas luego al agrupar siempre se toma la primera
+order by le desc ) prueba
 group by prueba.ID
 
 
